b3bd1bb0897dc28dc8388595a0339a7547b29ff8
Michael Fishman <michaeljfishman@gmail.com>
2019-12-26 21:53:11 -0500

    Started make_flower

--- mandala_utils.js
+++ mandala_utils.js
@@ -36,20 +36,63 @@ function simple_circle_flower_example(){
     var leaves = reflect_and_rotate(first_leaf, center_circle.position, 6)
 }
 
+function make_flower(layer = project.activeLayer){
+    // All the children of later have a bunch of undefined properties. Why?
+    _make_flower = function(pieces){
+        center_circle = new Path.Circle({
+            center : view.center,
+            radius : 30,
+            strokeColor: 'black',
+            strokeWidth: 1,
+            name: "center_circle"
+        });
+        // var petal_0 = pieces[0].clone({insert: false});
+        var petal_0 = pieces[0].clone({insert : false});
+        var petal_0_base = center_circle.bounds.topCenter;
+        set_base_point_position(petal_0, petal_0_base);
+        petal_0.pivot = petal_0_base;
+        console.log(petal_0);
+        layer.addChild(petal_0);
+        var num_petals = 3;
+        var angle = 360 / num_petals;
+        var i = 1;
+        for(i = 1; i < num_petals; i++){
+            petal = petal_0.clone({insert : false});
+
+            petal.pivot = center_circle.position;
+            // petal.position.x += 50 * i
+            console.log(i)
+            console.log(petal);
+            // petal.rotate(angle);
+            layer.addChild(petal);
+        }
+
+    }
+    var pieces = load_paths("resources/pieces.svg", null, _make_flower);
+
+
+
+
+}
+
 function get_base_point(path){
     // The point of the first segment is usually not the base point we want.
     // Assume for now that the item is vertical, and the bottom point is the base point
-    return path.bounds.bottomCenter
+    return path.bounds.bottomCenter;
 }
 
-
+// function set_base
 
 function set_base_point_position(path, point){
-    var adjusted_point = new Point(point.x, point.y - path.bounds.height/2);
-    path.position = adjusted_point
+    // Assume for now that the item is vertical, and the center of bottom bottom point is the base point
+    var base_point = get_base_point(path);
+    var adjustment_vector = new Point(path.position.x - base_point.x, path.position.y - base_point.y);
+    var adjusted_point = new Point(point.x + adjustment_vector.x, point.y + adjustment_vector.y);
+    path.position = adjusted_point;
 }
 
-function load_paths(file_path, targetLayer=null){
+
+function load_paths(file_path, targetLayer=null, _callback = null){
     var path_list = [];
     local_url = "http://localhost:8000/";
     url = local_url + file_path;
@@ -67,17 +110,33 @@ function load_paths(file_path, targetLayer=null){
                 });
                 for (var i = 0; i < paths.length; i++){
                     var p = paths[i];
-                    path_list.push(p)
+                    path_list.push(p);
                     if (targetLayer != null){
-                        targetLayer.addChild(p)
+                        targetLayer.addChild(p);
                     }
+                }
+                if(_callback != null){
+                    _callback(path_list);
                 }
             }
         });
 
-    tempLayer.remove()
-    activeLayer.activate()
-
-    return path_list
+    tempLayer.remove();
+    activeLayer.activate();
+    // if(_callback != null){
+    //     _callback(path_list);
+    // }
+    return path_list;
     
+}
+
+function load_paths_test(){
+    layer = project.activeLayer;
+    _callback = function(path_list){
+        console.log(path_list);
+        layer.addChild(path_list[0].clone());
+    };
+    var pieces = load_paths("resources/pieces.svg", null, _callback);
+    // console.log(pieces);
+    // project.activeLayer.addChild(pieces[0]);
 }
